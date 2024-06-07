4ddcb2390d8e7f79628b8d0d1f1a06dd10bb3193
divin-gakiza <divin.gakiza01@gmail.com>
2024-06-07 20:07:58 +0200

    form elements

---
+++ form.js
@@ -0,0 +1,7 @@
+console.log(document.forms.myForm);
+let myForm = document.forms.myForm;
+
+console.log(myForm.colour);
+console.log(myForm.biking.value);
+console.log(myForm.colour.value);
+console.log(myForm.siking.value);
