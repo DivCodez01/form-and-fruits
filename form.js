3b1fae062db8c5d6d49a180e55bcdb693f9b9333
divin-gakiza <divin.gakiza01@gmail.com>
2024-06-07 20:13:23 +0200

    myForm onfocus

--- form.js
+++ form.js
@@ -5,3 +5,10 @@ console.log(myForm.colour);
 console.log(myForm.biking.value);
 console.log(myForm.colour.value);
 console.log(myForm.siking.value);
+
+console.log(myForm.button)
+
+myForm.biking.onfocus = function () {
+    myForm.biking.style.border = "4px solid pink";
+    myForm.biking.style.outline = "none";
+}
