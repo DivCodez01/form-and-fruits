f3a77070ae811854f779311fe9180ff9cb332363
divin-gakiza <divin.gakiza01@gmail.com>
2024-06-07 20:39:05 +0200

    use jquery

---
+++ changeColorUsejquery.js
@@ -0,0 +1,6 @@
+//     var parent = document.querySelector("main").getElementsByTagName("ul")[0];
+//     var child = parent.getElementsByTagName("li")[0];
+
+//     let removed = parent.removeChild(child);
+
+$("li").css({ color: "red" });
